0baeac1eeeb380d0d64d694da250ca6e0e3daf90
Maoxu Li <li@maoxuli.com>
2021-08-16 15:49:47 -0400

    add install scripts

---
+++ system_install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash 
+set -e 
+
+## check if current user in docker group 
+echo "Add current user to group docker"
+sudo usermod -aG docker ${USER}
+
+## enable docker service 
+echo "Enable docker service"
+sudo systemctl enable docker 
+
+sudo apt-get update
+
+## install docker compose with pip3 
+echo "Install docker-compose"
+sudo apt-get install -y python3-pip 
+sudo pip3 install --upgrade pip 
+sudo pip3 install setuptools docker-compose
+
+## install inotify-tools 
+echo "Install extra tools"
+sudo apt-get install -y inotify-tools
+
+set +e 
